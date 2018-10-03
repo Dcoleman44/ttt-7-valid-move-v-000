79c7296a1174e3442024c0db2ec97d6b1b0c4ab4
dcoleman44 <dcoleman44@outlook.com>
2018-10-03 12:52:37 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,9 +9,9 @@ def position_taken?(board, index)
 end
 
 def Valid_move?(board, index)
-  if position_taken? == false && index.between?(0, 8) == true
+  if position_taken? == false && index.between?(0,8) == true
     true
-  elsif position_taken? == true || index.between?(0, 8) == false
+  elsif position_taken? == true || index.between?(0,8) == false
     false
   end
 end
